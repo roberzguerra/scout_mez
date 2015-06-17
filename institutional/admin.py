750a1e664715c47c3df21c2bd0e2842c184bd76f
roberzguerra <roberzguerra@gmail.com>
2015-06-17 17:49:12 -0300

    commit projeto

---
+++ institutional/admin.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+
+from django.contrib import admin
+from mezzanine.blog.admin import BlogCategoryAdmin
+from mezzanine.blog.models import BlogCategory
+from mezzanine.conf import settings
+
---
+++ institutional/models.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from mezzanine.pages.models import Page, RichText
+
+# class Homepage(Page):
+#     """
+#
+#     """
+#     class Meta:
+#         verbose_name = _(u"Homepage")
+#         verbose_name_plural = _(u"Homepages")
